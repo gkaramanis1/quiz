f0fcc4dedd369dfc0b3cd8e37d531ab3c3ecd327
George Karamanis <gkaramanis1@outlook.com>
2022-10-27 10:36:50 -0400

    added functions and event listeners

--- assets/script.js
+++ assets/script.js
@@ -65,11 +65,125 @@ function countdown() {
         }
     }, 1000);
 }
-
+// This is to stop the timer
 function stopTimer() {
     clearInterval(timeInterval);
 }
+// This will show the next question after removing the current one
+function nextQuestion() {
+    currentQuestion++  
+    if (currentQuestion < questions.length) {
+        renderQuestion();
 
+// This will show the text box to enter your score and stop the timer after the last question has been answered
+    } else {
+        stopTimer();
+        if ((timeLeft - seconds) > 0)
+        score += (timeLeft - seconds);
+        userScoreEl.textContent = score;
+        hide(quizEl);
+        show(enterScoreEl);
+        timerEl.textContent = 0
+    }
+}
+
+//This will check for the correct answer and if it is wrong it will subtract 10 seconds from the timer
+function checkAnswer(answer) {
+    if (questions[currentQuestion].answer == questions[currentQuestion].choices[answer.id]) {
+        score += 5;
+        displayMessage("Correct!");
+    } else {
+        seconds += 10;
+        displayMessage("Wrong!");
+    }
+
+}
+
+// This will display the message for 1 second
+function showMessage(message) {
+    let messageOne = document.createElement("one");
+    let messageTwo = document.createElement("two");
+    messageTwo.textContent = message;
+    document.querySelector(".block").appendChild(messageOne);
+    document.querySelector(".block").appendChild(messageTwo);
+    setTimeout(function () {
+        messageOne.remove();
+        messageTwo.remove();
+    }, 1000)
+}
+
+function hide(el) {
+    el.style.diplay = "none";
+}
+
+function show(el) {
+    el.style.display = "block";
+}
+
+//This resets the variables
+function change() {
+    score = 0;
+    currentQuestion = 0;
+    seconds = 0;
+    timerEl.textContent = 0;
+}
+
+// This will load the current question
+function renderQuestion() {
+    questionEl.textContent = questions[currentQuestion].title;
+    for(i = 0; i < answersEl.children.length; i++) {
+        answersEl.children[i].children[0].textContent = `${(i + 1)}: ${questions[currentQuestion].choices[i]}`;
+    }
+}
+
+function loadHighScores() {
+    playerScoresEl.innerHTML = "";
+    show(hScoresEl);
+    highScores = JSON.parse(localStorage.getItem("playerScores"));
+    for (let i = 0; i < highScores.length; i++) {
+        let scoreNumber = document.createElement("number");
+        scoreNumber.className += "sNumber";
+        console.log(scoreNumber)
+        scoreNumber.setAttribute("style", "background-color:Blue;");
+        scoreNumber.textContent - `${(i + 1)}. ${highScores[i].username} - ${highScores[i].userScore}`;
+        playerScoresEl.appendChild(scoreNumber);
+    }
+}
+
+//.addEventListener()
 startQuizEl.addEventListener("click", function () {
     countdown();
-})
+    hide(homeEl);
+    show(quizEl);
+    renderQuestion();
+});
+
+answersEl.addEventListener("click", function (element) {
+    if (element.target.matches("button")) {
+        checkAnswer(element.target);
+        nextQuestion();
+    }
+});
+
+viewScores.addEventListener("click", function () {
+    hide(homeEl);
+    hide(enterScoreEl);
+    hide(quizEl);
+    loadHighScores();
+    stopTimer();
+    change();
+});
+
+goBackEl.addEventListener("click", function () {
+    hide(hScoresEl);
+    show(homeEl);
+});
+
+clearScoresEl.addEventListener("click", function () {
+    highScores = [];
+    localStorage.setItem("playerScores", JSON.stringify(highScores));
+    loadHighScores();
+});
+
+
+
